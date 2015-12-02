f04fa6ec7d64aa17b50c53f05542b14e131136d6
Gilden00 <connor.r.mcneely@gmail.com>
2015-12-02 16:10:42 -0600

    Added the command /basic. Setup TestPlugin.java
    Added onEnable to io.github.gilden00.TestPlugin.TestPlugin
    Added onDisable to io.github.gilden00.TestPlugin.TestPlugin
    Added onCommand to io.github.gilden00.TestPlugin.TestPlugin
    	Add the /basic command
    
    Added the /basic command to plugin.yml

--- TestPlugin/src/main/java/io/github/gilden00/TestPlugin/TestPlugin.java
+++ TestPlugin/src/main/java/io/github/gilden00/TestPlugin/TestPlugin.java
@@ -1,14 +1,30 @@
 package io.github.gilden00.TestPlugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TestPlugin extends JavaPlugin{
 	@Override
 	public void onEnable(){
-		
+		getLogger().info("Hey, is this thing on?");
 	}
 	
 	public void onDisable(){
-		
+		getLogger().info("So long, farewell, auf wiedersehen, goodnight!");
+	}
+	
+	/**
+	 * called by bukkit when the player types "/". Must be registered in plugin.yml
+	 * @return true - nothing happens
+	 * @return false - player receives a message explaining how to use the command.
+	 */
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[]args){
+		if(cmd.getName().equalsIgnoreCase("basic")){
+			getLogger().info(sender+" issued basic using the alias "+label+".");
+			return true;
+		}
+		return false;
 	}
 }
