f30a762c768c254aa82e67cecc0abef9e77ff7b5
Gilden00 <connor.r.mcneely@gmail.com>
2015-12-02 14:42:51 -0600

    Added src/main/java/io/github/gilden00/TestPlugin/TestPlugin.java Added src/main/resources/UsefulLinks.txt

---
+++ TestPlugin/src/main/java/io/github/gilden00/TestPlugin/TestPlugin.java
@@ -0,0 +1,14 @@
+package io.github.gilden00.TestPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestPlugin extends JavaPlugin{
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+}
