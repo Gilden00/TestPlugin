7c7f1fa8e327282d6e77c1d7545b60f881cba724
Gilden00 <connor.r.mcneely@gmail.com>
2015-12-03 23:39:50 -0600

    Signed-off-by: Gilden00 <connor.r.mcneely@gmail.com>

--- TestPlugin/src/main/java/io/github/gilden00/TestPlugin/TPCommandExecutor.java
+++ TestPlugin/src/main/java/io/github/gilden00/TestPlugin/TPCommandExecutor.java
@@ -15,15 +15,46 @@ public class TPCommandExecutor implements CommandExecutor{
 
 	/**
 	 * called by bukkit when the player types "/". Must be registered in plugin.yml
-	 * @return true - nothing happens
+	 * @return true - Command syntax was correct.
 	 * @return false - player receives a message explaining how to use the command.
 	 */
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
+		//Basic
 		if(cmd.getName().equalsIgnoreCase("basic")){
+			return doBasic(sender, alias, args);
+		}
+		//Basic2
+		else if(cmd.getName().equalsIgnoreCase("basic2")){
+			return doBasic2(sender, alias, args);
+		}
+		return false;
+	}
+	
+	/**
+	 * This is a demo command.
+	 * @param sender - What issued the command.
+	 * @param alias - The alias of the command name.
+	 * @param args - Returns false if args.length > 0
+	 * @return True - Syntax was correct.
+	 */
+	public boolean doBasic(CommandSender sender, String alias, String[] args){
+		if(args.length == 0){
 			plugin.getLogger().info(sender+" issued basic using the alias "+alias+".");
 			return true;
-		}else if(cmd.getName().equalsIgnoreCase("basic2")){
+		}
+		return false;
+	}
+	
+	/**
+	 * This is a demo command that can only be issued by a player.
+	 * @param sender - What issued the command.
+	 * @param alias - The alias of the command name.
+	 * @param args - Returns false if args.length > 0
+	 * @return True - Syntax was correct.
+	 */
+	public boolean doBasic2(CommandSender sender, String alias, String[] args){
+		if(args.length == 0){
 			if(!(sender instanceof Player)){
 				sender.sendMessage("This command can only be issued by a player.");
 			}else{
@@ -33,5 +64,4 @@ public class TPCommandExecutor implements CommandExecutor{
 		}
 		return false;
 	}
-
 }
